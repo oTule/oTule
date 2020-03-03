45c75519ed78e3b9b5b49b97a5634cb8b1e5be0d
Artur Vrublevskiy <v8a8rt@gmail.com>
2020-03-03 23:22:04 +0300

    datepicker reworked

--- js/main.js
+++ js/main.js
@@ -25,3 +25,19 @@ function owlUpdateSlide(event) {
     var item      = event.item.index;
     $('.owlCustomCurrentNumber').text(item+1);
 }
+
+$('.events-filter__datepicker-input').on('focusin', function () {
+    $(this).closest('.datepicker-btn').addClass('picker-active');
+    console.log($(this).closest('.datepicker-btn'));
+    
+});
+
+$('.events-filter__datepicker-input').on('focusout', function () {    
+    if ($(this).val().length <= 0) {
+        $(this).closest('.datepicker-btn').removeClass('picker-active');
+    }
+});
+
+$('.events-filter__datepicker-reset').on('click', function () {
+    $(this).closest('.datepicker-btn').removeClass('picker-active');
+});
